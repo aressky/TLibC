b77e1ef67dd12fcd02bf5a848bcecc8004f8af9f
randyliu <randyliu@foxmail.com>
2014-01-17 10:41:40 +0800

    去掉警告

--- include/core/tlibc_list.h
+++ include/core/tlibc_list.h
@@ -1,60 +1,50 @@
 #ifndef _H_TLIBC_LIST
 #define _H_TLIBC_LIST
 
-//while(0)会导致条件判断为常量的警告
-#pragma warning (disable:4127)
-
 typedef struct _TLIBC_LIST_HEAD
 {
 	struct _TLIBC_LIST_HEAD *next, *prev;
 }TLIBC_LIST_HEAD;
 
 #define tlibc_list_init(_head)\
-do\
 {\
 	(_head)->next = _head;\
 	(_head)->prev = _head;\
-}while(0)
+}
 
 #define __tlibc_list_add(_new, _prev, _next)\
-do\
 {\
 	(_new)->next = (_next);\
 	(_new)->prev = (_prev);\
 	(_new)->next->prev = (_new);\
 	(_new)->prev->next = (_new);\
-}while(0)
+}
 
 #define tlibc_list_add(_new, _head)\
-do\
 {\
 	__tlibc_list_add((_new), (_head), ((_head)->next));\
-}while(0)
+}
 
 #define tlibc_list_add_tail(_new, _head)\
-do\
 {\
 	__tlibc_list_add((_new), (_head)->prev, (_head));\
-}while(0)
+}
 
 #define __tlibc_list_del(_prev, _next)\
-do\
 {\
 	(_next)->prev = _prev;\
 	(_next)->prev->next = (_next);\
-}while(0)
+}
 
 #define tlibc_list_del(entry)\
-do\
 {\
 	__tlibc_list_del((entry)->prev, (entry)->next);\
-}while(0)
+}
 
 #define tlibc_list_fix(_prev, _next)\
-do\
 {\
 	(_prev)->next = _next;\
 	(_prev)->next->prev = _prev;\
-}while(0)
+}
 
 #endif
