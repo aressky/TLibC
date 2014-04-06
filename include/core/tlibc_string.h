3940d69d610d06e750e752b244ab6a9497a06c72
小星星 <randyliu@foxmail.com>
2014-04-06 18:29:59 +0800

    增加错误提示字符串

---
+++ include/core/tlibc_string.h
@@ -0,0 +1,10 @@
+#ifndef _H_TLIBC_STRING
+#define _H_TLIBC_STRING
+
+#include "platform/tlibc_platform.h"
+#include "core/tlibc_error_code.h"
+
+const char *tstrerror(TLIBC_ERROR_CODE errno);
+
+#endif//_H_TLIBC_ERRNO
+
